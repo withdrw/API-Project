293a3bd7c06478340bc0aa5eb3c6c43ce1e80468
withdrw <finesse@usa.com>
2024-03-27 14:46:22 -0500

    Added Security

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+require("express-async-errors");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const app = express();
+const { environment } = require("./config");
+const isProduction = environment === "production";
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json());
+
+// backend/app.js
+const routes = require('./routes');
+
+// ...
+
+
+
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+  );
+
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+    );
+
+    app.use(routes); // Connect all the routes
+// backend/app.js
+// ...
+
+module.exports = app;

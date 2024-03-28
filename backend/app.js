d6bc5e634da575bf2b85d40bc9dd96ebcfd1920d
withdrw <finesse@usa.com>
2024-03-28 16:03:00 -0500

    Added user login / logout and Input Validation on user backend endpoint

--- backend/app.js
+++ backend/app.js
@@ -5,17 +5,47 @@ const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-const app = express();
+
 const { environment } = require("./config");
 const isProduction = environment === "production";
+
+const app = express();
+
 app.use(morgan("dev"));
+
 app.use(cookieParser());
 app.use(express.json());
 
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
 // backend/app.js
-const routes = require('./routes');
+const routes = require("./routes");
 
 // ...
+
+app.use(routes); // Connect all the routes
 
 // backend/app.js
 // ...
@@ -30,7 +60,7 @@ app.use((_req, _res, next) => {
 
 // backend/app.js
 // ...
-const { ValidationError } = require('sequelize');
+const { ValidationError } = require("sequelize");
 
 // ...
 
@@ -42,51 +72,24 @@ app.use((err, _req, _res, next) => {
     for (let error of err.errors) {
       errors[error.path] = error.message;
     }
-    err.title = 'Validation error';
+    err.title = "Validation error";
     err.errors = errors;
   }
   next(err);
 });
 
-// Security Middleware
-if (!isProduction) {
-  // enable cors only in development
-  app.use(cors());
-}
-
-// helmet helps set a variety of headers to better secure your app
-app.use(
-  helmet.crossOriginResourcePolicy({
-    policy: "cross-origin"
-  })
-  );
-
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
-    // backend/app.js
+// backend/app.js
 // ...
 // Error formatter
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
   res.json({
-    title: err.title || 'Server Error',
+    title: err.title || "Server Error",
     message: err.message,
     errors: err.errors,
-    stack: isProduction ? null : err.stack
+    stack: isProduction ? null : err.stack,
   });
 });
-
-    app.use(routes); // Connect all the routes
-// backend/app.js
-// ...
 
 module.exports = app;
